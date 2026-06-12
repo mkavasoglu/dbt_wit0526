b47056332c91d2e897832b1ea2d702d7a6070af4
mehmetcankavasoglu@gmail.com <mehmetcankavasoglu@gmail.com>
2026-06-12 07:14:47 +0000

    first commit

---
+++ models/example/my_first_dbt_model.sql
@@ -0,0 +1,19 @@
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+{{ config(materialized="table") }}
+
+with source_data as (
+
+        select 2 as id
+        union all
+        select 5 as id
+
+    )
+
+select *
+from source_data
